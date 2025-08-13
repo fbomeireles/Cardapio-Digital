eb1e4024935430551e17fdffe6fbac9b6d3d6156
Fabio Meireles <fabio.meirelesdsilva@mercadolivre.com>
2025-08-13 09:37:52 -0300

    Inclusão de handler de edicao

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
-	"Cardapio-Digital/internal/cli"
+	"Cardapio-Digital/handlers"
+	"Cardapio-Digital/internal/database"
+	"Cardapio-Digital/internal/repository"
+	"fmt"
+	"net/http"
+	"os"
 )
 
 func main() {
 
-	cli.Menu()
+	dsn := "root:senhasecreta@tcp(127.0.0.1:3306)/restaurante"
+	db, err := database.NewMySQLDB(dsn)
+	if err != nil {
+		fmt.Println("Não foi possível conectar ao banco")
+		os.Exit(1)
+	}
+	repo := &repository.IngredienteRepository{DB: db}
+
+	http.HandleFunc("/ingredientes", handlers.CriarIngredienteHandler(repo))
+
+	fmt.Println("Servidor rodando em http://localhost:8080/")
+	http.ListenAndServe(":8080", nil)
+
+	// cli.Menu()
 
 }
